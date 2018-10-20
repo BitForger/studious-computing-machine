5ae01c67c00400d49439baa1a70e9be1c44b43da
Noah Kovacs <kovacsn66@gmail.com>
2018-10-20 15:04:26 -0500

    refactor: remove params logging
    Signed-off-by: Noah Kovacs <kovacsn66@gmail.com>

--- app/controllers/aws_info_controller.rb
+++ app/controllers/aws_info_controller.rb
@@ -13,9 +13,6 @@ class AwsInfoController < ApplicationController
   end
 
   def view_detail
-    params.each do |key, value |
-      Rails.logger.warn "Param #{key}: #{value}"
-    end
     @s3 = Aws::S3::Resource.new(region: 'us-east-1')
 
     @bucket = @s3.bucket(params[:name])
